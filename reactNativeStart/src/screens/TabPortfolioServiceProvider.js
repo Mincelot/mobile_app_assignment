71e2c4a873d969f4640fbdf0298b8dc17104455e
Akanksha Shelat <Akanksha@Akankshas-MacBook-Pro.local>
2018-03-05 18:15:16 -0500

    portfolio list updated.

--- reactNativeStart/src/screens/TabPortfolioServiceProvider.js
+++ reactNativeStart/src/screens/TabPortfolioServiceProvider.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import { Text, StyleSheet, View, ScrollView, TextInput } from 'react-native';
+import { Text, StyleSheet, View, ScrollView, TextInput, FlatList } from 'react-native';
 import defaultStyles from '../../src/styles/default';
 import colors from '../styles/color';
 import { Card } from 'react-native-elements';
+import firebase from 'firebase';
+// import { FlatList } from 'react-native-gesture-handler';
 
-class TabPortfolio extends React.Component {
+class TabPortfolioServiceProvider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: ''};
+    this.state = {picFolders: [] }; //folder has text + picUrl 
+    this.user = null;
+  }
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged( user => {
+      if (user) {
+        this.user = user;
+        const rootRef = firebase.database().ref().child("users");
+        const infoRef = rootRef.child('info');
+        const userRef = infoRef.child(this.user.uid);
+        const picRef = userRef.child('picFolder');
+        
+        picRef.once('value')
+
+        .then((snapshot) => {
+          var picTemp = [];
+          if (snapshot.val()){
+            snapshot.forEach((item) => {
+              // console.log(child.key, child.val()); 
+              picTemp.push({
+                text: item.val().text,
+                picture: item.val().picUrl
+              });
+            });
+            this.setState({ picFolders: picTemp});
+          }
+        })
+        .catch((error) => {
+          this.setState({ status: error.message });
+        })
+      }
+
+    });
+  }
+  onTextChange(returnText, item){
+    var tempPicFolders = this.state.picFolders;
+    for(var i=0; i<tempPicFolders.length; i++ ){
+      if(tempPicFolders[i] == item){
+          tempPicFolders[i].text = returnText;
+      }
+    }
+    this.state.picFolders = tempPicFolders;
   }
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
-        <Card
-          image={{uri:"https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}>
-          <TextInput
-            style={{height: 40}}
-            placeholder="Description.."
-            onChangeText={(text) => this.setState({text})}
-          />
-        </Card>
+        <ScrollView> 
+          <FlatList
+            data={this.state.picFolders}
+            keyExtractor={(item, index) => index}
+            renderItem={({item}) =>
+            <Card
+              image={{uri:item.picture}}>
+              <TextInput
+                style={{height: 40}}
+                placeholder="Description..."
+                value={item.text} //original text 
+                //returnText is the new one, and we assign it back to item.text
+                onChangeText={this.onTextChange(item)}
+              />
+            </Card>} 
+          />           
         </ScrollView>
       </View>
     ); 
@@ -34,4 +84,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default TabPortfolio;
+export default TabPortfolioServiceProvider;
