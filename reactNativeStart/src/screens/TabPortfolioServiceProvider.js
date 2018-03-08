86b530fd2bf7bc1511979a6a5f9f891d41bd2ada
Akanksha Shelat <Akanksha@Akankshas-MacBook-Pro.local>
2018-03-08 03:24:36 -0500

    added a button to upload, need to fix text change.

--- reactNativeStart/src/screens/TabPortfolioServiceProvider.js
+++ reactNativeStart/src/screens/TabPortfolioServiceProvider.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet, View, ScrollView, TextInput, FlatList } from 'react-native';
+import { Text, StyleSheet, View, ScrollView, TextInput, FlatList, Modal, TouchableOpacity } from 'react-native';
 import defaultStyles from '../../src/styles/default';
 import colors from '../styles/color';
-import { Card,Header, Icon, Button } from 'react-native-elements';
+import UploadButton from '../components/UploadButton';
+import { Card,Header, Icon, Button, FormInput } from 'react-native-elements';
 import firebase from 'firebase';
 // import { FlatList } from 'react-native-gesture-handler';
 
 class TabPortfolioServiceProvider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {picFolders: [], uploadPicture: false}; //folder has text + picUrl 
+    this.state = {picFolders: [], modalVisible: false, tempUrl: '', testDescription: ''}; //folder has text + picUrl 
     this.isViewMode = this.props.isView ? this.props.isView : false;
     this.user = null;
   }
@@ -28,7 +29,6 @@ class TabPortfolioServiceProvider extends React.Component {
           var picTemp = [];
           if (snapshot.val()){
             snapshot.forEach((item) => {
-              // console.log(child.key, child.val()); 
               picTemp.push({
                 description: item.val().text,
                 picture: item.val().picUrl
@@ -44,58 +44,118 @@ class TabPortfolioServiceProvider extends React.Component {
 
     });
   }
-  onTextChange(returnText, item){
-    var tempPicFolders = this.state.picFolders;
+  onTextChange(returnText, item, tempPicFolders){
+    
+    //var tempPicFolders = this.state.picFolders;
     for(var i=0; i<tempPicFolders.length; i++ ){
       if(tempPicFolders[i] == item){
-          tempPicFolders[i].text = returnText;
+          tempPicFolders[i].description = returnText;
       }
     }
-    this.state.picFolders = tempPicFolders;
+    this.setState({picFolders: tempPicFolders, testDescription: returnText});
   }
   componentWillUnmount() {
     this.unsubscribe();
   }
 
-   renderuploadButton(){
-  //   <Icon
-  //     reverse
-  //     // raised
-  //     name='control-point'
-  //     type='font-awesome'
-  //     color='#f50'
-  //     onPress={() => console.log('hello')} 
-  //     /
-      <Text>Upload things</Text>
-   }
+  uploadPicture(){
+    this.setState({ modalVisible: false ,picFolders: [...this.state.picFolders,{ picture: this.state.tempUrl, description: ''} ]}, () => {
+      this.setState({ tempUrl : '' });
+    });   
+  }
+
+  setModalVisible() {
+    this.setState({modalVisible: false});
+  }
 
   render() {
     return (
       <View style={styles.container}>
+      <Text>{this.state.testDescription}</Text>
         <Header
-          //leftComponent={{ icon: 'menu', color: '#fff' }}
           centerComponent={{ text: 'My Portfolio', style: { color: '#fff', fontSize: 30, fontStyle: "italic" } }}
-          rightComponent={{ icon: 'control-point', color: '#fff', size: 30, /*onPress: renderuploadButton() */ }}
-          //rightComponent={this.renderuploadButton()}
+          rightComponent={<Icon
+            name='control-point'
+            color='#fff'
+            size={40}
+            onPress={() => {this.setState({ modalVisible: true})}}
+          /> }         
           outerContainerStyles={{ backgroundColor: colors.tabNavBackground }}
         />
         <ScrollView>
           {/* <Text>{this.isViewMode ? 'ViewMode' : 'EditMode, Erase this after.'}</Text> */}
-          <FlatList
-            data={this.state.picFolders}
-            keyExtractor={(item, index) => index}
-            renderItem={({item}) =>
-            <Card
-              image={{uri:item.picture}}>
-              <TextInput
-                style={{height: 40}}
-                placeholder="Description..."
-                value={item.description} //original text 
-                //returnText is the new one, and we assign it back to item.description
-                onChangeText={this.onTextChange(item)}
+          <View>     
+            {!this.isViewMode && //view mode false = chef user  
+              <FlatList
+                data={this.state.picFolders}
+                keyExtractor={(item, index) => index}
+                renderItem={({item}) =>
+                <View style={styles.container}>
+                  <Card
+                    image={{uri:item.picture}}>
+                    <TextInput
+                      style={{height: 40}}
+                      placeholder="Description..."
+                      value={item.description} //original text 
+                      //returnText is the new one, and we assign it back to item.description
+                      onChangeText={this.onTextChange.bind(item,this.state.picFolders)}
+                    />
+                  </Card>
+                </View>}  
               />
-            </Card>} 
-          />           
+            } 
+          </View> 
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {
+              alert('Modal has been closed.');
+            }}>
+            <View style={{height: '100%', width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
+              <View style={styles.centeredDialog}>
+                <Text style={[styles.labelText, {fontSize: 14}]}>Please enter the direct link of your picture:</Text>
+                <FormInput
+                  placeholder='Tap here to edit'
+                  value={this.state.tempUrl ? this.state.tempUrl : ''}
+                  onChangeText={(newUrl) => this.setState({ tempUrl: newUrl })}
+                >
+                </FormInput>
+                <View style={[{display: 'flex'}, {flexDirection: 'row'}, {justifyContent: 'space-between'}]}>
+                <TouchableOpacity
+                    style={[styles.myButton]}
+                    onPress={this.uploadPicture.bind(this)}>
+                    <Text style={{color: '#7E8F7C'}}> Confirm </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.myButton}
+                    onPress={this.setModalVisible.bind(this)}>
+                    
+                    <Text style={{color: '#7E8F7C'}}> Cancel </Text>
+                </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+          {/* <View>     
+            {this.isViewMode && //view mode false = chef user  
+              <FlatList
+                data={this.state.picFolders}
+                keyExtractor={(item, index) => index}
+                renderItem={({item}) =>
+                <View style={styles.container}>
+                  <Card
+                    image={{uri:item.picture}}>
+                    <Text>
+                      {item.description}
+                    </Text>
+                  </Card> 
+                </View>}
+              />
+            } 
+          </View>                   */}
+
+
         </ScrollView>
       </View>
     ); 
@@ -105,6 +165,25 @@ class TabPortfolioServiceProvider extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  myButton: {
+    alignItems: 'center',
+    padding: 10,
+    borderWidth:2,
+    backgroundColor:"#FDF3E7",
+    borderColor:"#f3753f",
+    borderRadius: 15
+
+  },
+  centeredDialog: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: '30%',
+    backgroundColor: '#FDF3E7',
+    borderWidth:2,
+    borderColor:"#f3753f",
+    borderRadius: 10
   }
 
 });
