de312416da00b225fc4e51ccc1a1d033ee8c09d0
Akanksha Shelat <akankshashelat12@gmail.com>
2018-03-29 14:33:13 -0400

    temporary message form added.

--- reactNative/src/screens/MessageForm.js
+++ reactNative/src/screens/MessageForm.js
@@ -4,20 +4,104 @@ import { StyleSheet, View } from 'react-native';
 import colors from '../styles/color';
 
 class MessageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {cuisine: '', date: '', partySize: '', price: ''}; 
+    if (this.props && this.props.navigation && this.props.navigation.state && this.props.navigation.state.params) {
+      // this.isViewMode = this.props.navigation.state.params.isView ? true : false;
+      this.userUidPassedIn = this.props.navigation.state.params.selectedUserUid;
+      this.loggedInClient = this.props.navigation.state.params.loggedInClient;
+    }
+    this.unsubscribe = null;
+  }
+
+  backButton() {
+    this.props.navigation.dispatch(NavigationActions.back());
+  }
+
   render() {
     return (
-        <View style={styles.container}>
-           
+      <View style={styles.container}>
+        <View><Text style={{color:'red'}}>{this.state.status}</Text></View>
+    
+          <View style={{width:'95%'}}>
+            <FormLabel labelStyle={styles.textColor}>Date</FormLabel>
+            <FormInput 
+              value={this.state.date}
+              onChangeText={date => this.setState({ date })}
+              />
+            <FormLabel labelStyle={styles.textColor}>Cuisine</FormLabel>
+            <FormInput 
+              secureTextEntry={true}
+              value={this.state.cuisine}
+              onChangeText={cuisine => this.setState({ cuisine })}
+              />
+            <FormLabel labelStyle={styles.textColor}>Number of people</FormLabel>
+            <FormInput
+              value={this.state.partySize}
+              onChangeText={partySize => this.setState({ partySize })}
+              />
+            <FormLabel labelStyle={styles.textColor}>Price</FormLabel>
+            <FormInput
+              value={this.state.price}
+              onChangeText={price => this.setState({ price })}
+              />
         </View>
+
+        <View style={styles.buttonContainer}>
+          <View>
+            <Button 
+              buttonStyle={styles.buttonColor}
+              title="Send"
+              onPress={}
+              borderRadius={5}
+              />
+          </View>
+          <View>
+            <Button 
+              buttonStyle={styles.buttonColor}
+              title="Back"
+              onPress={this.backButton.bind(this)}
+              borderRadius={5}
+              />
+          </View>
+        </View>
+      </View>
     ); 
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: colors.background
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
+    backgroundColor: colors.backgroundColor
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  accountContainer: {
+    flex: 1
+  },
+  buttonColor: {
+    backgroundColor: colors.alternatePurple
+  },
+  textColor: {
+    color: 'black'
+  },
+  headerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerStyle: {
+    // color: colors.backgroundColor,
+    fontSize: 36, 
+    fontWeight: 'bold'
+  }
+
 });
 
 export default MessageForm;
