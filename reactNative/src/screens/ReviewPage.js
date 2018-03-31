ac047fe5c713acec3e0b0c3dbe684d37b180a812
syarotta1997 <helenlu1997@gmail.com>
2018-03-31 00:33:21 -0400

    added average rating to chef all reviews page

--- reactNative/src/screens/ReviewPage.js
+++ reactNative/src/screens/ReviewPage.js
@@ -5,6 +5,7 @@ import colors from '../styles/color';
 import defaultStyles from '../../src/styles/default';
 import { NavigationActions } from "react-navigation";
 import { Header, Icon, Button, ListItem, Card, List, Divider } from 'react-native-elements';
+import StarRating from 'react-native-star-rating';
 import firebase from 'firebase';
 import NavigatorService from '../services/navigator';
 import { connect } from 'react-redux';
@@ -27,7 +28,7 @@ class ReviewPage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {reviews: []};
+        this.state = {reviews: [], rating:0, num_rating:0};
         if (this.props && this.props.navigation && this.props.navigation.state && this.props.navigation.state.params) {
           // this.isViewMode = this.props.navigation.state.params.isView ? true : false;
           this.userUidPassedIn = this.props.navigation.state.params.selectedUserUid;
@@ -47,8 +48,26 @@ class ReviewPage extends React.Component {
             const infoRef = rootRef.child('info');
             const userRef = infoRef.child(this.userUidPassedIn);
             const reviews = userRef.child('reviews');
+            const ratingRef = userRef.child('rating');
+            const num_ratingRef = userRef.child('num_rating');
             //const chefs = infoRef.orderByChild("isAccountTypeClient").equalTo(false).limitToLast(100);
             //alert(reviews);
+            var cur_rating = 0;
+            var num_rating = 0;
+            ratingRef.once('value')
+            .then((snapshot) => {
+              if  (snapshot.val()) {
+                cur_rating = snapshot.val();
+                this.setState({rating: cur_rating});
+              }
+            })
+            num_ratingRef.once('value')
+            .then((snapshot) => {
+              if  (snapshot.val()) {
+                num_rating = snapshot.val();
+                this.setState({num_rating:num_rating});
+              }
+            })
             reviews.once('value')
             .then((snapshot) => {
               let reviewsTemp = [];
@@ -110,6 +129,15 @@ class ReviewPage extends React.Component {
                  centerComponent={{ text: 'Reviews', style: { color: '#fff', fontSize: 30, fontStyle: "italic" } }}
                  outerContainerStyles={{ backgroundColor: colors.tabNavBackground }}
             />
+        </View>
+        <View style={{width:'60%',alignItems:'center',marginLeft:'20%'}}>
+        <StarRating
+                disabled={true}
+                maxStars={5}
+                rating={this.state.rating/this.state.num_rating}
+                emptyStarColor={colors.tabNavBackground}
+                fullStarColor={colors.tabNavBackground}
+              />
         </View>
            <ScrollView>
                <FlatList
